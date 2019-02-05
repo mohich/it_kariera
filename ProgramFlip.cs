f894b12fd90d70ba718c37cbf867e30961926fa8
mohich <mohich@abv.bg>
2019-02-05 12:40:41 +0200

    Add files via upload
    first commit

---
+++ ProgramFlip.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int panichka = 0;
+            int len = arr.Length;
+            for (int i = 0; i < arr.Length/2; i++)
+            {
+                panichka = arr[i];
+                arr[i] = arr[arr.Length - 1 - i];
+                arr[arr.Length - 1 - i] = panichka;
+            }
+
+         Console.WriteLine(String.Join("; ", arr)); 
+
+
+        }
+    }
+}
